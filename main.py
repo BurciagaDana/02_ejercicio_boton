e890a1c6d2b7cb5a2e5cba5f3bcb5632c6d953a5
Burciaguita <23308060610289@cetis61.edu.mx>
2026-04-20 16:47:58 -0600

    a

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import flet as ft
 
-# función principal
+
 def main(page: ft.Page):
     page.title = "App con botón"
 
-    # texto inicial
+    
     mensaje = ft.Text("Hola, mundo desde Flet!", size=25, color="pink")
 
-    # función del botón
+    
     def cambiar_texto(e):
         mensaje.value = "¡Adios, noob :V!"
         mensaje.color = "green"
         page.update()
 
-    # botón
+    
     boton1 = ft.ElevatedButton("Cambiar mensaje", on_click=cambiar_texto)
 
-    # agregar a la ventana
+    
     page.add(mensaje, boton1)
 
-# ejecutar app
+
 ft.app(target=main)
